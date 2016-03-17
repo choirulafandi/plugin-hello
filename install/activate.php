c76425e60dd836cfe4f53e43ef302cd6925f7b33
Choirul Afandi <science.afandi@gmail.com>
2016-03-17 20:48:00 +0700

    default value installer

--- install/activate.php
+++ install/activate.php
@@ -15,9 +15,7 @@ class PeepSoHelloInstall extends PeepSoInstall
 		$settings = PeepSoConfigSettings::get_instance();
 		$settings->set_option('peepso_helloworld_use_custom_message', 0);
 		$settings->set_option('peepso_helloworld_custom_message', 'Hello From Afandi');
-		$settings->set_option('peepso_helloworld_custom_message', 'Hello From Afandi');
-		$settings->set_option('peepso_helloworld_exclamation_marks', 1);
-		$settings->set_option('peepso_helloworld_align', 'left');
+		$settings->set_option('peepso_helloworld_custom_user_id', 1);
 
 
 		parent::plugin_activation();
